57dbf4aa3caaca082c470b3957deaf6bd14bb134
Dmitry Riabokon <99822054+DmitriyR90@users.noreply.github.com>
2025-01-24 21:37:30 +0200

    made spining

---
+++ js/spiner.js
@@ -0,0 +1,36 @@
+function spinColumn(columnId, duration, finalPosition) {
+  const spinner = document.getElementById(columnId);
+
+  const finalOffset = finalPosition * -185.623;
+  spinner.style.transition = `top ${duration}s ease-in-out`;
+  spinner.style.top = `${finalOffset}px`;
+}
+
+document.getElementById('spinButton').addEventListener('click', () => {
+  // Disable the spin button during the animation
+  const spinButton = document.getElementById('spinButton');
+  spinButton.disabled = true;
+
+  spinColumn('spinner1', 2, 4);
+  spinColumn('spinner2', 3, 4);
+  spinColumn('spinner3', 4, 6);
+
+  setTimeout(() => {
+    let sevenSlots = document.querySelectorAll('#seven');
+    console.log(sevenSlots);
+
+    for (let item of sevenSlots) {
+      console.log(item);
+      item.classList.add('slot-win');
+    }
+  }, 4000);
+
+  setTimeout(() => {
+    document.getElementById('winModal').classList.add('active');
+    spinButton.disabled = false;
+  }, 5000);
+});
+
+document.getElementById('closeModal').addEventListener('click', () => {
+  document.getElementById('winModal').classList.remove('active');
+});

6bfa03bd3b1d4e8fd889f553c01e11279de50089
Dmitry Riabokon <99822054+DmitriyR90@users.noreply.github.com>
2025-01-31 00:49:03 +0200

    2 steps for responsive

--- js/spiner.js
+++ js/spiner.js
@@ -1,13 +1,29 @@
+function getScrollHeight(elementId) {
+  const element = document.getElementById(elementId);
+  if (element) {
+    let computedStyle = getComputedStyle(element);
+    let marginBottom = parseFloat(computedStyle.marginBottom);
+    let height = parseFloat(computedStyle.height);
+
+    return marginBottom + height;
+  } else {
+    console.error(`Element with id "${elementId}" not found.`);
+    return 0;
+  }
+}
+
 function spinColumn(columnId, duration, finalPosition) {
   const spinner = document.getElementById(columnId);
 
-  const finalOffset = finalPosition * -185.623;
+  let scrollHeight = getScrollHeight('slotItem');
+  console.log('Scroll Height:', scrollHeight);
+
+  const finalOffset = finalPosition * -scrollHeight;
   spinner.style.transition = `top ${duration}s ease-in-out`;
   spinner.style.top = `${finalOffset}px`;
 }
 
 document.getElementById('spinButton').addEventListener('click', () => {
-  // Disable the spin button during the animation
   const spinButton = document.getElementById('spinButton');
   spinButton.disabled = true;
 

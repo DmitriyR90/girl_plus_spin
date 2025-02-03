0ec14397d06c81772e9e5fd3eb7279f613168b09
Dmitry Riabokon <99822054+DmitriyR90@users.noreply.github.com>
2025-02-03 19:54:44 +0200

    spinner more speed

--- js/spiner.js
+++ js/spiner.js
@@ -27,9 +27,9 @@ document.getElementById('spinButton').addEventListener('click', () => {
   const spinButton = document.getElementById('spinButton');
   spinButton.disabled = true;
 
-  spinColumn('spinner1', 2, 4);
-  spinColumn('spinner2', 3, 4);
-  spinColumn('spinner3', 4, 6);
+  spinColumn('spinner1', 1.5, 11);
+  spinColumn('spinner2', 1.2, 13);
+  spinColumn('spinner3', 1.3, 15);
 
   setTimeout(() => {
     let sevenSlots = document.querySelectorAll('#seven');
@@ -39,13 +39,13 @@ document.getElementById('spinButton').addEventListener('click', () => {
       console.log(item);
       item.classList.add('slot-win');
     }
-  }, 4000);
+  }, 1800);
 
   setTimeout(() => {
     document.getElementById('slotWindow').style.display = 'none';
     document.getElementById('winModal').classList.add('active');
     spinButton.disabled = false;
-  }, 5000);
+  }, 3000);
 });
 
 document.getElementById('closeModal').addEventListener('click', () => {
